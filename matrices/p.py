023549ae929ea382470d26a5b890f70700beecfd
19pa1a0405 <60500567+19pa1a0405@users.noreply.github.com>
2022-10-15 12:16:35 +0530

    Update p.py

--- matrices/p.py
+++ matrices/p.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import sympy as sym
 
 def line_gen(A,B):
    len =10
@@ -19,70 +20,38 @@ import shlex
 #end if
 
 
+
 #input parameters
-a=5                     #length of base (CD)
-b=4                     #length of side (CB)
-theta = np.pi/3         #angle between CB & CD
-C = np.array([0,0])     #Vertex C
-B = np.array([a,0])
-D = b*np.array([(np.cos(theta)),(np.sin(theta))])
-A = B-C+D
-l=D+B     
-O=l/2
-x=B-O
-y=B-A
-ar1=0.5*np.linalg.norm((np.cross(D,O)))
-ar2=0.5*np.linalg.norm((np.cross(x,y)))
-
-
-k=C-B
-l=C-D
-m=A-D
-n=A-B
-
-ar3=0.5*np.linalg.norm((np.cross(k,l)))
-ar4=0.5*np.linalg.norm((np.cross(n,B)))
-ar5=0.5*np.linalg.norm((np.cross(m,n)))
-print("Ar(DOC)=",ar1)
-print("Ar(AOB)=",ar2)
-print("Ar(DCB)=",ar3)
-print("Ar(ACB)=",ar4)
-print("Ar(DAB)=",ar5)
-##Generating all lines
+A = np.array([2,-3]) 
+B = np.array([-2,1])
+C = np.array([4.5,0])
+X = (A+B+C)/3
+##Generating all line
 x_AB = line_gen(A,B)
 x_BC = line_gen(B,C)
-x_CD = line_gen(C,D)
-x_DA = line_gen(D,A)
+x_CA = line_gen(C,A)
 
-x_OA = line_gen(O,A)
-x_OB = line_gen(O,B)
-x_OC = line_gen(O,C)
-x_OD = line_gen(O,D)
-
-
+x=np.linspace(-5,5.5,20)
+y=(1-(2*x))/3
+plt.plot(x,y)
+h=np.linspace(-5,5,20)
+k=(9-(2*h))/2
+plt.plot(h,k)
 #Pliotting all lines
 plt.plot(x_AB[0,:],x_AB[1,:])
 plt.plot(x_BC[0,:],x_BC[1,:])
-plt.plot(x_CD[0,:],x_CD[1,:])
-plt.plot(x_DA[0,:],x_DA[1,:])
-
-plt.plot(x_OA[0,:],x_OA[1,:])
-plt.plot(x_OB[0,:],x_OB[1,:])
-plt.plot(x_OC[0,:],x_OC[1,:])
-plt.plot(x_OD[0,:],x_OD[1,:])
-
-
+plt.plot(x_CA[0,:],x_CA[1,:])
 #Labeling the coordinates
-tri_coords = np.vstack((A,B,C,D,O)).T
+tri_coords = np.vstack((A,B,C,X)).T
 plt.scatter(tri_coords[0,:], tri_coords[1,:])
-vert_labels = ['A','B','C','D','O']
+vert_labels = ['A','B','C','X']
 for i, txt in enumerate(vert_labels):
-    plt.annotate(txt,                                   
-                 (tri_coords[0,i], tri_coords[1,i]),   
-                 textcoords="offset points",            
+    plt.annotate(txt,                               
+                 (tri_coords[0,i], tri_coords[1,i]),    
+                 textcoords="offset points",        
                  xytext=(5,-15),                      
                  ha='left')                           
-
+plt.plot(h,k)
 plt.xlabel('$x-axis$')
 plt.ylabel('$y-axis$')
 plt.legend(loc='best')
